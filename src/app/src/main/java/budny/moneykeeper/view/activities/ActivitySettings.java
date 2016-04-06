4c19dc7e7da83c9ef719ccd8788259f6ad982655
Raman Budny <budnyjj@gmail.com>
2016-04-06 22:50:40 +0300

    add basic prefs

--- src/app/src/main/java/budny/moneykeeper/view/activities/ActivitySettings.java
+++ src/app/src/main/java/budny/moneykeeper/view/activities/ActivitySettings.java
@@ -1,14 +1,41 @@
 package budny.moneykeeper.view.activities;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
 
 import budny.moneykeeper.R;
 
-public class ActivitySettings extends AppCompatActivity {
+public class ActivitySettings extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+//        setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.preference_headers, target);
+    }
+
+    public static class Prefs1Fragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            //PreferenceManager.setDefaultValues(getActivity(), R.xml.preference_advanced, false);
+            addPreferencesFromResource(R.xml.preferences_fragmented);
+        }
+    }
+
+    public static class Prefs2Fragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences_dependencies);
+        }
     }
 }
